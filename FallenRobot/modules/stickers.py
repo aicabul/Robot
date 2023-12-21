95008c2a6e71349c0f60871790a0bc1fba98c127
unknown <150268045+aicabul@users.noreply.github.com>
2023-12-21 10:14:17 +0700

    Update stickers.py
    Signed-off-by: unknown <150268045+aicabul@users.noreply.github.com>

--- FallenRobot/modules/stickers.py
+++ FallenRobot/modules/stickers.py
@@ -132,7 +132,7 @@ def kang(update: Update, context: CallbackContext):
         elif msg.reply_to_message.sticker and msg.reply_to_message.sticker.emoji:
             sticker_emoji = msg.reply_to_message.sticker.emoji
         else:
-            sticker_emoji = "🤔"
+            sticker_emoji = "🤨"
 
         if not is_animated:
             try:
@@ -278,7 +278,7 @@ def kang(update: Update, context: CallbackContext):
                 png_sticker = urlemoji[1]
                 sticker_emoji = urlemoji[2]
             except IndexError:
-                sticker_emoji = "🤔"
+                sticker_emoji = "🤨"
             urllib.urlretrieve(png_sticker, kangsticker)
             im = Image.open(kangsticker)
             maxsize = (512, 512)
